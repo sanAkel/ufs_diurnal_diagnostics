63a6264ca296e1f57e06adc65c091a8028254e16
sanAkel <santha.akella@gmail.com>
2025-01-03 01:30:06 +0000

    to get files from hpss

---
+++ RTOFS/score-card/data-prep/get_files.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+import yaml
+import argparse
+import pandas as pd
+
+import warnings
+warnings.filterwarnings('ignore') 
+# --
+
+from utils import rtofs_hpss_path
+
+# user inputs
+get_inputs = argparse.ArgumentParser(prog='\nget_files.py',\
+             description='To gather RTOFS nowcast and forecast files from HPSS', usage='%(prog)s [options]')
+
+get_inputs.add_argument('--config_file', type=str,\
+                        help='yaml file that sets configuration, see provided yaml file for an example',\
+                        default=\
+                        '/collab1/data/Santha.Akella/skinSST_vs_NSST/scripts/ufs_diurnal_diagnostics/RTOFS/score-card/data-prep/config-get_data.yaml')
+args = get_inputs.parse_args()
+# --
+
+config_file = args.config_file
+
+config = yaml.load( open( config_file, "r"), Loader=yaml.FullLoader)
+exp_labels = config['exp_labels']
+
+nEXP = len(exp_labels)
+
+start_date, end_date = [pd.to_datetime(config['start_date']), pd.to_datetime(config['end_date'])]
+data_dates=pd.date_range(start_date, end_date, freq='D')
+
+print("\n\nMake sure you have loaded module: hpss\n\n")
+cwd = os.getcwd()
+print("Current path:\n{}".format(cwd))
+
+# Gather data from experiment
+for iexp in range(nEXP):
+  exp_name  = config['%s'%(exp_labels[iexp])]['name']
+  data_path = config['%s'%(exp_labels[iexp])]['path']
+  print("\n\nGathering data from: ", exp_name)
+
+  oPath = config['output_path'] + exp_name
+  if os.path.exists( oPath):
+
+      for data_date in data_dates:
+         output_dir = oPath + "/"+ "{}".format(data_date.strftime('%Y%m%d'))
+         subprocess.run( ["mkdir", "-p", "{}".format(output_dir)], check=True, capture_output=True)
+         os.chdir( output_dir)
+         #print(output_dir)
+
+         cmd0 = rtofs_hpss_path( exp_name, config['%s'%(exp_labels[iexp])]['path'],\
+                          data_date,\
+                          config['%s'%(exp_labels[iexp])]['tar_file_pref'],\
+                          config['%s'%(exp_labels[iexp])]['tar_file_suff'])
+
+         cmd_n = cmd0 + ' ' + './' + config['nowcast_file_pref'] + '*' + config['nowcast_file_suff']
+         cmd_f = cmd0 + ' ' + './' + config['fcstcast_file_pref']+ '*' + config['fcstcast_file_suff']
+         print(cmd0)
+
+         stat1 = os.system("htar -xvf " + cmd_n)
+         if stat1 == 0:
+           print("Fetched nowcast files from {}".format(data_date.strftime('%Y%m%d')))
+
+         stat2 = os.system("htar -xvf " + cmd_f)
+         if stat2 == 0:
+           print("Fetched forecast files from {}".format(data_date.strftime('%Y%m%d')))
---
+++ RTOFS/score-card/data-prep/utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+
+def rtofs_hpss_path(exp_name, path, date, tar_file_pref, tar_file_suff):
+
+  if exp_name == "v2.4":
+    # If v2.4, form a string like:
+    # /NCEPPROD/1year/hpssprod/runhistory/rh2024/202411/20241105/com_rtofs_v2.4_rtofs.20241105.nc.tar
+    cmd = path +\
+         'rh{}/'.format(date.strftime('%Y')) +\
+         '{}/'.format(date.strftime('%Y%m')) +\
+         '{}/'.format(date.strftime('%Y%m%d')) +\
+         tar_file_pref + '{}'.format(date.strftime('%Y%m%d')) + tar_file_suff
+
+  elif exp_name == "v2.5":
+    # form a string like:
+    # emc-ocean/5year/Dan.Iredell/rtofs.v2.5.test01/rtofs.20241225/rtofs.ncgrb.tar
+    cmd = path +\
+         'rtofs.{}/'.format(date.strftime('%Y%m%d')) +\
+         tar_file_pref + tar_file_suff
+         #tar_file_pref + '{}'.format(date.strftime('%Y%m%d')) + tar_file_suff
+
+  else:
+    sys.exit("\n\nUnknown exp_name. Fix and try again\n\n")
+
+  return cmd
+
